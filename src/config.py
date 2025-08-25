3391bbdb310787fed76355826f42964d04432999
capacstefan <stefancapac9@gmail.com>
2025-08-25 16:59:48 +0300

    Few changes in path logic ( used os library) in config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(SRC_DIR)
 DATA_DIR = os.path.join(BASE_DIR, "data")
 MODELS_DIR = os.path.join(BASE_DIR, "models")
 
